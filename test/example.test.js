87d5b08290a89aade7ee05782cb6398cf7a58baa
Cecile Muller <cecilemuller@users.noreply.github.com>
2020-06-13 16:09:58 +0200

    test: add a Mocha tests suite

---
+++ test/example.test.js
@@ -0,0 +1,18 @@
+/* eslint-env node, mocha */
+"use strict";
+const {strictEqual, deepStrictEqual} = require("assert");
+const MyLibrary = require("../src/MyLibrary");
+
+describe("Tests Suite 1", function(){
+	it("MyLibrary is a function", function(){
+		strictEqual(typeof MyLibrary, "function");
+	});
+	it("MyLibrary returns the expected value", function(){
+		const actual = MyLibrary(123);
+		const expected = {
+			text: "Hello World",
+			number: 123
+		};
+		deepStrictEqual(actual, expected);
+	});
+});
